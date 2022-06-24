04c1d2abc38fdaa0f3fc485ea68a9e78b963c2a3
Cristian Tironi <ctironi@estudiante.ceibal.edu.uy>
2022-06-24 19:18:06 -0300

    Segundo commit: Proyecto listo

--- src/main/java/com/sofka/proyect/Proyecto_Bienvenido.java
+++ src/main/java/com/sofka/proyect/Proyecto_Bienvenido.java
@@ -1,4 +1,18 @@
 package com.sofka.proyect;
 
+import javax.swing.*;
+
 public class Proyecto_Bienvenido {
+    public static void main(String[] args) {
+        String nombre;
+        String celular;
+        String edad;
+
+        nombre = JOptionPane.showInputDialog("Ingresar el Nombre de Usuario");
+        celular = JOptionPane.showInputDialog("Ingresar el Numero de Celular");
+        edad = JOptionPane.showInputDialog("Ingresar la Edad del Usuario");
+
+        JOptionPane.showMessageDialog(null, "Bienvenido Señor " + nombre + ", es un placer para nosotros contar con una persona de " + edad + " años.\n\"Proximamente nos comunicaremos con usted al numero " + celular + ".\n\"Feliz día");
+    }
+
 }
